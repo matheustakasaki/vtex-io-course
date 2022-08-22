6564b8aced37069a4cb18a159fa41a0b34edfb50
matheustakasaki <matheus8antunes@gmail.com>
2022-08-22 19:42:16 -0300

    Aprendendo como tornar o componente react em um componente customizavel via site editor e internacionalizar seu conteúdo e propriedades

--- custom-store-block/react/Countdown.tsx
+++ custom-store-block/react/Countdown.tsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { TimeSplit } from './typings/global'
+import { FormattedMessage } from 'react-intl'
+import { tick, getTwoDaysFromNow } from './utils/time'
+import { useCssHandles } from 'vtex.css-handles'
 
-interface CountdownProps { }
 
-const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ }) => {
+interface CountdownProps {
+  targetDate: string,
+  title: string
+}
+
+const CSS_HANDLES = ['container', 'countdown', 'title']
+const DEFAULT_TARGET_DATE = getTwoDaysFromNow()
+
+const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ title, targetDate = DEFAULT_TARGET_DATE }) => {
+  const [timeRemaining, setTime] = useState<TimeSplit>({
+    hours: '00',
+    minutes: '00',
+    seconds: '00',
+  });
+
+  const handles = useCssHandles(CSS_HANDLES)
+  const titleText = title || <FormattedMessage id={"countdown.title"} />
+  tick(targetDate, setTime)
+
   return (
-    <h1>Componente de Countdown Teste</h1>
+    <div className={`${handles.container} t-heading-2 fw3 w-100 c-muted-1`}>
+      <div className={`${handles.title} db tc`}>{titleText}</div>
+      <div className={`${handles.countdown} db tc`}>
+        {`${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}`}
+      </div>
+    </div>
   )
 }
 
@@ -12,7 +38,19 @@ Countdown.schema = {
   title: 'editor.countdown.title',
   description: 'editor.countdown.description',
   type: 'object',
-  properties: {},
+  properties: {
+    title: {
+      title: 'Sou um título',
+      type: 'string',
+      default: null,
+    },
+    targetDate: {
+      title: 'Data final',
+      description: 'Data final utilizada no contador',
+      type: 'string',
+      default: null,
+    }
+  },
 }
 
 export default Countdown
